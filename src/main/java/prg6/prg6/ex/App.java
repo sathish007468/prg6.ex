1506281c7923f81bc839ea013c4d3aa166085324
SATHISHG <satish@cambridge.edu.in>
2025-06-07 07:34:00 +0530

    Moved project files to root

---
+++ src/main/java/prg6/prg6/ex/App.java
@@ -0,0 +1,13 @@
+package prg6.prg6.ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
